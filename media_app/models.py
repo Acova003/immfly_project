eae5d9becdfdc924a5ddddb84ae47676d7c17194
Amee <ameecova@yahoo.com>
2023-04-05 01:12:52 +0200

    Create models and hide myenv

--- media_app/models.py
+++ media_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Channel(models.Model):
+    title = models.CharField(max_length=255)
+    language = models.CharField(max_length=2)
+    picture = models.ImageField(upload_to='channel_pictures/')
+    description = models.ManyToManyField('description', blank=True)
+    maturity_rating = models.CharField(max_length=255)
+    spoken_lang = models.CharField(max_length=255)
+    caption_lang = models.CharField(max_length=255)
+
+class Movie(Channel):
+    duration = models.PositiveIntegerField()
+    genre = models.CharField(max_length=255)
+    
+
+class TVShow(Channel):
+    num_seasons = models.PositiveIntegerField()
+    
+class Season(models.Model):
+    number = models.PositiveIntegerField()
+    tvshow = models.ForeignKey(TVShow, on_delete=models.CASCADE)
+
+class Episode(models.Model):
+    number = models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
